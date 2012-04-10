2fdb7bcf3e80e345bd5c955adbeaaa34449b8392
Visnu Pitiyanuvath <visnupx@gmail.com>
2012-04-10 14:33:07 -0700

    fixing some egregious bugs

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ http.createServer(function(req, res) {
   var u = url.parse(req.url, true).query.url
 
   if (u) {
+    console.log(u)
     request({url: u, encoding: null}, function(err, resp, data) {
-      if (err) return console.log(err)
+      if (err) return res.end(err)
 
       var canvas = new Canvas(d, d)
         , ctx = canvas.getContext('2d')
@@ -28,16 +29,17 @@ http.createServer(function(req, res) {
         , saturated = new HSLA(0, 0, 0, 1)
         , hsla
       for (var i = 0; i < p.length; i += 4) {
-        hsla = HSLA.fromRGBA({ r: p[0], g: p[1], b: p[2], a: p[3] })
+        hsla = HSLA.fromRGBA({ r: p[i], g: p[i+1], b: p[i+2], a: p[i+3] })
         if (hsla.s > saturated.s && 25 < hsla.l && hsla.l < 80)
           saturated = hsla
       }
 
-      delete hsla.hsla
+      delete saturated.hsla
       res.writeHead(200, {'Content-Type': 'application/json'})
-      res.end(JSON.stringify(hsla))
+      res.end(JSON.stringify(saturated))
     })
   } else {
-    res.end("Try http://saturated.jit.su/?url=http%3A%2F%2Fsphotos.xx.fbcdn.net%2Fhphotos-ash3%2Fs720x720%2F529205_413720861989482_125086767519561_1556562_910734765_n.jpg")
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end('<form><input name="url" placeholder="image url"/><input type="submit"/></form>')
   }
 }).listen(process.env.PORT || 8002)
